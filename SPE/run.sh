6cbda4cddacbaa105ef47ae0299f2c5f75e23c24
Yogesh Verma <yverma132@gmail.com>
2024-04-11 10:31:56 +0300

    Add files via upload

---
+++ SPE/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=TopNNs
+#SBATCH --time=15:14:00
+#SBATCH --account=project_2006852
+#SBATCH --mem-per-cpu=15G
+#SBATCH --output=Train_spe_zinc.out
+#SBATCH --partition=gpu
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=1
+
+
+python -u runner.py --config_dirpath ../configs/zinc --config_name SPE_gine_gin_mlp_pe37.yaml --seed 0
